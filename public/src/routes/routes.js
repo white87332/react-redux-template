7fdd9da3c1b4b84e778b9eb5e54f2ab395ed4028
Tony Jiang <white87332@gmail.com>
2017-04-14 17:00:40 +0800

    add i18n

--- public/src/routes/routes.js
+++ public/src/routes/routes.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
 import Main from '../components/main/main';
 import Layout from '../components/layout/layout';
 import asyncComponent from '../utils/asyncComponent';
+import i18n from '../i18n/i18n';
 
 export default function createRoutes()
 {
     return (
         <BrowserRouter>
-            <Main>
-                <Layout>
-                    <Route exact path="/" component={asyncComponent(() => System.import('../path/index/index').then(module => module.default))} />
-                    <Route exact path="/counter" component={asyncComponent(() => System.import('../path/counter/counter').then(module => module.default))} />
-                </Layout>
-            </Main>
+            <I18nextProvider i18n={i18n}>
+                <Main>
+                    <Layout>
+                        <Route exact path="/" component={asyncComponent(() => System.import('../path/index/index').then(module => module.default))} />
+                        <Route exact path="/counter" component={asyncComponent(() => System.import('../path/counter/counter').then(module => module.default))} />
+                    </Layout>
+                </Main>
+            </I18nextProvider>
         </BrowserRouter>
     );
 }

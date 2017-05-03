9df210fc211703c497c8614547fd2c12a1b5e5eb
Tony Jiang <white87332@gmail.com>
2017-05-03 17:26:14 +0800

    remove layout

--- public/src/routes/routes.js
+++ public/src/routes/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import Main from '../components/main/main';
-import Layout from '../components/layout/layout';
 import asyncComponent from '../utils/asyncComponent';
 import i18n from '../i18n/i18n';
 
@@ -12,10 +11,8 @@ export default function createRoutes()
         <BrowserRouter>
             <I18nextProvider i18n={i18n}>
                 <Main>
-                    <Layout>
-                        <Route exact path="/" component={asyncComponent(() => System.import('../containers/index/index').then(module => module.default))} />
-                        <Route exact path="/counter" component={asyncComponent(() => System.import('../containers/counter/counter').then(module => module.default))} />
-                    </Layout>
+                    <Route exact path="/" component={asyncComponent(() => System.import('../containers/index/index').then(module => module.default))} />
+                    <Route exact path="/counter" component={asyncComponent(() => System.import('../containers/counter/counter').then(module => module.default))} />
                 </Main>
             </I18nextProvider>
         </BrowserRouter>

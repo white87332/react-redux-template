79f18e08ba93f51d737e550080072ffd82036b07
Tony Jiang <white87332@gmail.com>
2017-05-16 15:32:13 +0800

    update

--- public/src/routes/routes.js
+++ public/src/routes/routes.js
@@ -7,13 +7,13 @@ export default function createRoutes()
 {
     return (
         <BrowserRouter>
-            <Switch>
-                <Main>
+            <Main>
+                <Switch>
                     <Route exact path="/" component={asyncComponent(() => System.import('../containers/index/index').then(module => module.default))} />
                     <Route exact path="/counter" component={asyncComponent(() => System.import('../containers/counter/counter').then(module => module.default))} />
                     <Route exact path="/counter/test" component={asyncComponent(() => System.import('../containers/test/test').then(module => module.default))} />
-                </Main>
-            </Switch>
+                </Switch>
+            </Main>
         </BrowserRouter>
     );
 }

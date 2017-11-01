f70c0cd9d41453b3efb6181588fec007bc4049ba
Tony Jiang <white87332@gmail.com>
2017-11-01 16:12:52 +0800

    update i18next

--- public/src/i18n/i18n.js
+++ public/src/i18n/i18n.js
@@ -3,15 +3,28 @@ import XHR from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n.use(XHR)
-    .use(LanguageDetector)// 偵測瀏覽器語系
+    .use(LanguageDetector)
     .init({
-        fallbackLng: 'en', // 未偵測到時的後備語系
+        whitelist: [
+            'en-us', 'zh-tw'
+        ],
+
+        fallbackLng: 'en-us', // 未偵測到時的後備語系
+
         ns: ['common'], // 語系的 loading namespace 如語系檔案名稱 common.js
+
         defaultNS: 'common', // 預設的 namespace name
+
         debug: false,
+
         interpolation: {
             escapeValue: false // not needed for react!!
         },
+
+        load: 'currentOnly',
+
+        lowerCaseLng: true,
+
         backend: {
             // 設定語系檔案的 server 路徑, 會以GET的方式跟 server 要檔案
             // lng = 語系代碼 ns = namespace

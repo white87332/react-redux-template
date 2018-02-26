68676a59a0943b67ffab2622734fce57e6df4c91
Tony Jiang <white87332@gmail.com>
2018-02-26 10:23:40 +0800

    update

--- public/src/epics/counterEpic.js
+++ public/src/epics/counterEpic.js
@@ -1,12 +1,10 @@
+/* eslint no-unused-vars: ["error", {"args": "Observable"}] */
 import { Observable } from 'rxjs';
 
 const counterEpic = (action$) =>
 {
     return action$.ofType('INCREMENT_COUNTER').mergeMap((action) => {
-
-        return Observable
-            .filter(action => action.type === 'DECREMENT_COUNTER')
-            .mapTo({ type: 'INCREMENT_COUNTER' });
+        return () => action;
     });
 };
 

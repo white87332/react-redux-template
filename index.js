c32d1d5be17eec8d360d9567e8eaf8d439143d09
Tony <white87332@gmail.com>
2018-03-03 06:28:34 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 80;
 
 app.use('/public', express.static(path.resolve('public')));
 

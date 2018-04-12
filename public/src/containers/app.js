1ac41df98fd159b0b35b00754b20cba0c586d0c9
Tony Jiang <white87332@gmail.com>
2018-04-12 10:28:27 +0800

    add injectGlobal

--- public/src/containers/app.js
+++ public/src/containers/app.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 import { I18nextProvider } from 'react-i18next';
+import { injectGlobal } from 'styled-components';
 import configureStore from '../store/configureStore';
 import createRoutes from '../routes/routes';
 import i18n from '../i18n/i18n';
@@ -17,6 +18,13 @@ const store = configureStore(middleware);
 
 // routes
 const routes = createRoutes();
+
+/* eslint no-unused-expressions: ["error", { "allowTaggedTemplates": true }] */
+injectGlobal`
+    body {
+        box-sizing: border-box;
+    }
+`;
 
 render(
     <Provider store={store}>

40f36f98ef859c19e34dbb953fec821e60a86569
Tony Jiang <white87332@gmail.com>
2018-04-30 09:14:15 +0800

    update

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -45,6 +45,10 @@ module.exports = {
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 use: 'url?limit=8192&name=./asset/img/[name].[ext]'
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2))(\?[a-z0-9]+)?$/,
+                loader: 'file',
             }
         ]
     },

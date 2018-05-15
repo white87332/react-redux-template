5b077d1476aba5dd6ce86adb48beed368d7053ea
Tony Jiang <white87332@gmail.com>
2018-05-15 10:48:33 +0800

    update package

--- public/src/routes/routes.js
+++ public/src/routes/routes.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Main from '../components/main/main';
 import asyncComponent from '../utils/asyncComponent';
 
 export default function createRoutes()
 {
     return (
-        <BrowserRouter>
-            <Main>
-                <Switch>
-                    <Route exact path="/" component={asyncComponent(() => import('../containers/index/index').then(module => module.default))} />
-                    <Route exact path="/threeLine" component={asyncComponent(() => import('../containers/threeLine/threeLine').then(module => module.default))} />
-                    <Route exact path="/counter" component={asyncComponent(() => import('../containers/counter/counter').then(module => module.default))} />
-                    <Route exact path="/video" component={asyncComponent(() => import('../containers/video/video').then(module => module.default))} />
-                </Switch>
-            </Main>
-        </BrowserRouter>
+        <Main>
+            <Switch>
+                <Route exact path="/" component={asyncComponent(() => import('../containers/index/index').then(module => module.default))} />
+                <Route exact path="/threeLine" component={asyncComponent(() => import('../containers/threeLine/threeLine').then(module => module.default))} />
+                <Route exact path="/counter" component={asyncComponent(() => import('../containers/counter/counter').then(module => module.default))} />
+                <Route exact path="/video" component={asyncComponent(() => import('../containers/video/video').then(module => module.default))} />
+            </Switch>
+        </Main>
     );
 }

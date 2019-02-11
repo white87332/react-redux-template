dfb3d026c7c459cfe1e6e083ed8b841b1799272d
Tony <white87332@gmail.com>
2019-02-11 14:56:29 +0800

    update

--- public/src/containers/counter/counter.js
+++ public/src/containers/counter/counter.js
@@ -1,3 +1,27 @@
+// import React, { useState } from 'react';
+// import { connect } from 'react-redux';
+//
+// function Counter()
+// {
+//     // Declare a new state variable, which we'll call "count"
+//     const [count, setCount] = useState(0);
+//
+//     return (
+//         <div>
+//             <p>
+//               You clicked
+//                 {count}
+//               times
+//             </p>
+//             <button onClick={() => setCount(count + 1)}>
+//                 Click me
+//             </button>
+//         </div>
+//     );
+// }
+//
+// export default connect()(Counter);
+
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';

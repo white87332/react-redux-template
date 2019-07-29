e9cfe6901be1ae012153033c470a3cb5e72fb319
Tony Jiang <white87332@gmail.com>
2019-07-29 11:24:39 +0800

    Update coinmarketcap.js

--- public/src/epics/coinmarketcap.js
+++ public/src/epics/coinmarketcap.js
@@ -1,4 +1,4 @@
-import { of, interval, Observable } from 'rxjs';
+import { of, from, interval, Observable } from 'rxjs';
 import { switchMap, catchError, map } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
 import { get } from 'superagent';
@@ -10,20 +10,22 @@ const fetchTickerEpic = (action$, state$) => {
         .pipe(
             ofType(types.FETCH_TICKER_REQ),
             switchMap((action) => {
-                return get(`https://api.coinmarketcap.com/v2/ticker/?limit=${action.query}`);
-            }),
-            map((res) => {
-                return tickActions.fetchTickerSuc(
-                    res.body.data
+                return from(get(`https://api.coinmarketcap.com/v2/ticker/?limit=${action.query}`))
+                .pipe(
+                    map((res) => {
+                        return tickActions.fetchTickerSuc(
+                            res.body.data
+                        );
+                    }),
+                    catchError((err) => {
+                        return of(
+                            tickActions.fetchTickerErr(
+                                err.response
+                            )
+                        );
+                    })
                 );
             }),
-            catchError((err) => {
-                return of(
-                    tickActions.fetchTickerErr(
-                        err.response
-                    )
-                );
-            })
         );
 };
 
